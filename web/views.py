abdc456e5e21b0adf9b50feb683eec64d2cbc4c2
CodePishaun <ojotemitopekayode8@gmail.com>
2026-04-14 09:21:05 +0100

    Add files via upload

---
+++ web/views.py
@@ -0,0 +1,16 @@
+
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class ProjectsView(TemplateView):
+    template_name = 'projects.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+
